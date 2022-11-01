ed51d4aed00a83cc27c09708cd65b2682fea6f68
Fernando Garcia <fgarcia@deezee.com>
2022-11-01 16:58:04 -0500

    Section 2.12 completed

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure.MappingViews;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -15,19 +16,11 @@ namespace Vidly.Controllers
             var movie = new Movie() { Name = "Shrek!" };
             return View(movie);
         }
-        public ActionResult Edit (int id)
-        {
-            return Content("id=" + id);
-        }
-        // movies
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
-            if (String.IsNullOrEmpty(sortBy))
-                sortBy = "Name";
 
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        public ActionResult ByReleaseDate (int year, int month)
+        {
+            
+            return Content(year + "/" + month);
         }
     }
 }
